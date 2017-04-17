7c4822bf03948fe1e1b99ad27fa469c7161669a2
JM Versteeg <janmerijnversteeg@gmail.com>
2017-04-17 16:53:53 +0200

    update readme

--- sandbox.js
+++ sandbox.js
@@ -9,6 +9,20 @@ const hobbySchema = {
     name: required(),
 };
 
+// Let's define a custom rule!
+// This rule will not validate a single field directly but instead look for the attributes "width" and "height"
+// On the other fields (second parameter passed to the rule validator)
+const resolution = ({minWidth, minHeight, error = 'Image too small.'}) => {
+    return (_, {width, height}) => width < minWidth || height < minHeight ? error : null;
+};
+
+// Because of the functional way rules are configured, you can make "templates" by using wrapping functions
+const highRes = (error = 'Image must be high-res!') => resolution({minWidth: 3200, minHeight: 2400, error});
+
+// Lets define a CONDITION (note conditions are different from rules)
+// that checks on the photo if the "subject" equals "cat"
+const condCat = (_, {subject}) => subject === 'cat';
+
 const photoSchema = {
     fileName:   format({
         pattern: /\.(jpe?g|png)$/i,
@@ -17,9 +31,7 @@ const photoSchema = {
     subject:    withIn(['cat', 'dog']),
     width:      required(),
     height:     required(),
-    resolution: cond((_, {subject}) => subject === 'cat', (_, {width, height}) => {
-        return width < 800 || height < 600 ? 'Cat photos must be hi-res.' : null;
-    }),
+    resolution: cond(condCat, highRes('Cat photos must be hi-res.')),
 };
 
 const passwordConstraint = all([
@@ -32,19 +44,14 @@ const passwordConstraint = all([
     ], {error: 'Should contain at least one digit, one lowercase letter and one uppercase letter.'}),
 ]);
 
+const minPhotos = min => length({min, minError: n => `Please upload at least ${n} photo(s).`,});
+
 const userSchema = {
-
     username: required(),
-
     password: passwordConstraint,
-
-    hobbies: [hobbySchema],
-
-    photos: all([
-        length({
-            min:      1,
-            minError: n => `Please upload at least ${n} photo(s).`,
-        }),
+    hobbies:  [hobbySchema],
+    photos:   all([
+        minPhotos(1),
         [photoSchema]
     ]),
 };
@@ -52,13 +59,13 @@ const userSchema = {
 const validate = validator(userSchema, {strict: true});
 
 const input = {
-    username: 'sgtlambda',
-    password: 'hunter2',
+    username:  'sgtlambda',
+    password:  'hunter2',
     dropTable: true,
-    hobbies:  [{
+    hobbies:   [{
         name: 'nitpicking',
     }],
-    photos:   [{
+    photos:    [{
         fileName: 'funny.jpg',
         subject:  'cat',
         width:    400,
